d993aa84b9f6193c3b488b7dc53567b3df4b4b96
Holly P <foxyyakuu@gmail.com>
2025-12-11 23:46:38 -0500

    Mongo

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:LNYOnYsFBnoCG8F80aHlF7ir@172.21.148.200:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
