32738b16a896b38e656efffd2f3cb4b9f5c8ff99
Holly P <foxyyakuu@gmail.com>
2025-12-11 23:55:26 -0500

    Mongo 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
 console.log(
-    "API_URL :",
-    API_URL
+    "https://hollypederse-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/ :",
+    https://hollypederse-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/
 );
